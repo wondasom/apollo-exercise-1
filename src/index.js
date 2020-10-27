bbf1237e62e88f72759e3d65bbe7f942728b05a3
dasomwon <wondasom93@gmail.com>
2020-10-27 17:04:11 +0100

    get movies query

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import { ApolloProvider } from '@apollo/react-hooks';
+import client from './routes/apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById('root')
+);
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,2 +1,18 @@
-export default () => 'Home';
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
+const GET_MOVIES = gql`
+	{
+		movies {
+			id
+			medium_cover_image
+		}
+	}
+`;
+
+export default () => {
+	const { loading, data } = useQuery(GET_MOVIES);
+	
+	return <h1>Home</h1>;
+};
